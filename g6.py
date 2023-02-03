05c3cd178707685efdfbf85220b1a07f88f70845
BGN656 <bgn656@gmail.com>
2023-02-03 23:29:14 +0200

    m9

--- g6.py
+++ g6.py
@@ -2,7 +2,6 @@ import math as ma
 
 x = float(input('Введите любое число:  '))
 
-
 print(x**5*56)
 print(x*ma.pi)
 print(x*ma.e)
--- main.py
+++ main.py
@@ -4,6 +4,10 @@ def print_hi(name):
 
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
+
+
+
+
 if __name__ == '__main__':
     print_hi('PyCharm')
 
